1eab7f2d9af57457ab40e44bbe8b056eaefd0de8
bhusanSahoo <bhusan12sahoo@gmail.com>
2025-04-29 11:25:50 +0530

    Converting delegates into the event

--- Assets/Scripts/Interactables/LightSwitchView.cs
+++ Assets/Scripts/Interactables/LightSwitchView.cs
@@ -8,7 +8,7 @@ public class LightSwitchView : MonoBehaviour, IInteractable
 
 
     public delegate void LightSwitchDelegate();  //signature -  signature is return type should be void and there should no parameter
-    public static LightSwitchDelegate lightSwitch;      //instance
+    public static event LightSwitchDelegate lightSwitch;      //instance
 
     private void Start() => currentState = SwitchState.Off;
 
